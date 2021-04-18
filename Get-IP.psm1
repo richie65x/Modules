513dcf7595c4516a88d5e9d9200094a6e859b7e7
richie65x <51408884+richie65x@users.noreply.github.com>
2021-04-18 15:10:50 -0400

    Add files via upload

---
+++ Get-IP.psm1
@@ -0,0 +1,44 @@
+﻿Function Get-IP {
+Write-Host "Fetching IP info..." -ForegroundColor Gray -BackgroundColor Black
+$AdapterList = @()
+Get-NetAdapter | ? {$_.InterfaceDescription -NOTmatch 'Bluetooth'}  | % { 
+$adapter = $_
+$prefix = (Get-NetIPAddress -InterfaceIndex $adapter.InterfaceIndex | ? {$_.ipv4address}).prefixlength
+('1' * $prefix).PadRight(32, '0') | Out-Null;$bitString=('1' * $prefix).PadRight(32,'0'); $SnM=[String]::Empty
+    for($i=0;$i -lt 32;$i+=8)
+    {$byteString=$bitString.Substring($i,8); $SnM+="$([Convert]::ToInt32($byteString, 2))."}
+If ((Get-NetIPInterface -InterfaceAlias $_.InterfaceAlias).Dhcp -eq "Enabled") {$DHCP = "DHCP"} else {$DHCP = "STATIC"}
+$AdapterList += ((Get-NetIPConfiguration | ? {$_.InterfaceIndex -match "$($adapter.InterfaceIndex)"} | select `
+    @{Name='IPAddress';Expression ={"$($_.IPv4Address.IPAddress) ($DHCP)"}},`
+    @{Name='SubnetMask';Expression ={"$($SnM.TrimEnd('.')) (/$prefix)"}},`
+    @{Name='Gateway';Expression ={$_.IPv4DefaultGateway.NextHop}},`
+    @{Name='DNSServer(s)';Expression ={$_.DNSServer.ServerAddresses}},`
+    @{Name='Alias';Expression ={$_.InterfaceAlias}},`
+    @{Name='Index';Expression ={$_.InterfaceIndex}},`
+    @{Name='Description';Expression ={$_.InterfaceDescription}},`
+    @{Name='Status';Expression ={$_.NetAdapter.Status}},`
+    @{Name='MAC';Expression ={$adapter.MacAddress}}))
+    }
+Write-Host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" -ForegroundColor Gray
+
+$AdapterList | % {
+$_ | % {
+If ($_.IPAddress -match "169.") {
+    $_.IPAddress = "($DHCP)"
+    $_.SubnetMask = " - "
+}
+
+$Item00 = $_
+($Item00 | Out-String).Split("`n|`r",[System.StringSplitOptions]::RemoveEmptyEntries) | % {
+If ($_ -match "Index") {Write-Host $_ -ForegroundColor cyan} 
+If ($_ -match "Alias") {Write-Host $_ -ForegroundColor Yellow} 
+If ($_ -match ": Up") {Write-Host $_ -ForegroundColor Green} 
+If ($_ -match ": Disconnected") {Write-Host $_ -ForegroundColor Magenta}
+If ($_ -NOTmatch "Index" -and $_ -NOTmatch "Alias" -and $_ -NOTmatch "Status") {Write-Host $_} }
+
+}
+Write-Host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" -ForegroundColor Gray
+}
+} # End Function Get-IP
+
+set-alias -name gip -value Get-IP
---
+++ KillPosh.psm1
@@ -0,0 +1,15 @@
+﻿Function KillPoSh {
+Write-Host "Enter the Process ID... " -ForegroundColor Green -BackgroundColor Black
+$PoSh_PIDs = $null; $PoSh_PIDs = (Get-Process | ? {$_.ProcessName -match "Powershell_"}).Id
+$PoSh_PIDs
+$PID_No = $null; Write-Host "  ID #: " -ForegroundColor Cyan -NoNewline ; $PID_No = (Read-Host).Trim()
+If ($PID_No) {
+$PoSh_Path = (Get-Process -Id $PID_No).Path
+Stop-Process -Id $PID_No
+Start-Sleep -Seconds 2
+& $PoSh_Path
+}
+If (!$PID_No) {Write-Host "Aborted!" -ForegroundColor Yellow}
+} # END Function killposh {
+
+set-alias -name kps -value KillPoSh
